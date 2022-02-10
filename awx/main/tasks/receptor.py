e10234c84bb827f7d5a8a903920a0f8fc32a1af7
Alan Rominger <arominge@redhat.com>
2022-02-10 14:27:40 -0500

    Handle case that canceling task releases work

--- awx/main/tasks/receptor.py
+++ awx/main/tasks/receptor.py
@@ -381,7 +381,11 @@ class AWXReceptorJob:
                     unit_status = receptor_ctl.simple_command(f'work status {self.unit_id}')
                     detail = unit_status.get('Detail', None)
                     state_name = unit_status.get('StateName', None)
-                except Exception:
+                except Exception as exc:
+                    if 'unknown work unit' in str(exc):
+                        self.task.instance.refresh_from_db()
+                        if self.task.cancel_flag:
+                            return AnsibleRunnerResult('canceled', 1)  # was canceled and work already released
                     detail = ''
                     state_name = ''
                     logger.exception(f'An error was encountered while getting status for work unit {self.unit_id}')
