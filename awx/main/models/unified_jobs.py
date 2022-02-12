5074cca9bb4b6196f5a9ac3c36f5cc23d262e0b2
Alan Rominger <arominge@redhat.com>
2022-02-12 09:07:19 -0500

    Move task to on_commit for race conditions

--- awx/main/models/unified_jobs.py
+++ awx/main/models/unified_jobs.py
@@ -1407,9 +1407,13 @@ class UnifiedJob(
                     cancel_fields.append('job_explanation')
                 self.save(update_fields=cancel_fields)
                 self.websocket_emit_status("canceled")
-            from awx.main.tasks.system import cancel_unified_job
 
-            cancel_unified_job.apply_async([self.id], queue=self.get_queue_name())
+            def actually_cancel():
+                from awx.main.tasks.system import cancel_unified_job
+
+                cancel_unified_job.apply_async([self.id], queue=self.get_queue_name())
+
+            connection.on_commit(actually_cancel)
         return self.cancel_flag
 
     @property
