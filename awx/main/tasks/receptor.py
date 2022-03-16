21907534e21cb8c9a9bffa2253a3ff8d433e0276
Alan Rominger <arominge@redhat.com>
2022-03-16 09:58:49 -0400

    Undo part of diff

--- awx/main/tasks/receptor.py
+++ awx/main/tasks/receptor.py
@@ -280,7 +280,6 @@ class AWXReceptorJob:
                     receptor_ctl.simple_command(f"work release {self.unit_id}")
                 except Exception:
                     logger.exception(f"Error releasing work unit {self.unit_id}.")
-            receptor_ctl.close()
 
     @property
     def sign_work(self):
@@ -354,6 +353,8 @@ class AWXReceptorJob:
             res = list(first_future.done)[0].result()
             if res.status == 'canceled':
                 receptor_ctl.simple_command(f"work cancel {self.unit_id}")
+                resultsock.shutdown(socket.SHUT_RDWR)
+                resultfile.close()
                 # TODO: abort without status transition, recover later by restarting the processing step
                 self.task.instance.refresh_from_db(fields=['cancel_flag'])
                 if not self.task.instance.cancel_flag:
