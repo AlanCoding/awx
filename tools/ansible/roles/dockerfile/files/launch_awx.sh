ed49d259189e24bb2addaf9c6ec95dff9b6deadd
Shane McDonald <me@shanemcd.com>
2022-03-25 08:15:37 -0400

    Fix race condition causing rsyslog to crash

--- tools/ansible/roles/dockerfile/files/launch_awx.sh
+++ tools/ansible/roles/dockerfile/files/launch_awx.sh
@@ -19,4 +19,11 @@ wait-for-migrations
 
 awx-manage provision_instance
 
+# This file will be re-written when the dispatcher calls reconfigure_rsyslog(),
+# but it needs to exist when supervisor initially starts rsyslog to prevent the
+# container from crashing. This was the most minimal config I could get working.
+cat << EOF > /var/lib/awx/rsyslog/rsyslog.conf
+action(type="omfile" file="/dev/null")
+EOF
+
 supervisord -c /etc/supervisord.conf
