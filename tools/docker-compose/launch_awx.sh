536898e23d5b6d28975fcf2ad628fe1081248e45
Shane McDonald <me@shanemcd.com>
2022-03-29 12:15:16 -0400

    Remove rsyslog.conf from image
    Follows the same pattern as tools/ansible/roles/dockerfile/files/launch_awx.sh

--- tools/docker-compose/launch_awx.sh
+++ tools/docker-compose/launch_awx.sh
@@ -3,6 +3,13 @@ set +x
 
 bootstrap_development.sh
 
+# This file will be re-written when the dispatcher calls reconfigure_rsyslog(),
+# but it needs to exist when supervisor initially starts rsyslog to prevent the
+# container from crashing. This was the most minimal config I could get working.
+cat << EOF > /var/lib/awx/rsyslog/rsyslog.conf
+action(type="omfile" file="/dev/null")
+EOF
+
 cd /awx_devel
 # Start the services
 exec make supervisor
