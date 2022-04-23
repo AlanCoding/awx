32c6d7c2ce80e6cf7992eb17119e71d783c87ebf
Alan Rominger <arominge@redhat.com>
2022-04-22 21:39:56 -0400

    bump migration

--- awx/main/migrations/0160_add_notifications_processed_field.py
+++ awx/main/migrations/0160_add_notifications_processed_field.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('main', '0158_make_instance_cpu_decimal'),
+        ('main', '0159_deprecate_inventory_source_UoPU_field'),
     ]
 
     operations = [
