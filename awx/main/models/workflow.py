f1a9ccd9459f66d568f1735daec8201b2a432446
Alan Rominger <arominge@redhat.com>
2022-05-25 22:25:55 -0400

    Remove property that is now field

--- awx/main/models/workflow.py
+++ awx/main/models/workflow.py
@@ -257,10 +257,6 @@ class WorkflowJobNode(WorkflowNodeBase):
             models.Index(fields=['identifier']),
         ]
 
-    @property
-    def event_processing_finished(self):
-        return True
-
     def get_absolute_url(self, request=None):
         return reverse('api:workflow_job_node_detail', kwargs={'pk': self.pk}, request=request)
 
@@ -626,10 +622,6 @@ class WorkflowJob(UnifiedJob, WorkflowJobOptions, SurveyJobMixin, JobNotificatio
     def workflow_nodes(self):
         return self.workflow_job_nodes
 
-    @property
-    def event_processing_finished(self):
-        return True
-
     def _get_parent_field_name(self):
         if self.job_template_id:
             # This is a workflow job which is a container for slice jobs
@@ -806,10 +798,6 @@ class WorkflowApproval(UnifiedJob, JobNotificationMixin):
         self.save(update_fields=['started'])
         self.send_approval_notification('running')
         return can_start
-
-    @property
-    def event_processing_finished(self):
-        return True
 
     def send_approval_notification(self, approval_status):
         from awx.main.tasks.system import send_notifications  # avoid circular import
