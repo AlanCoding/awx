be8781a7defb80205ed86a075372b1eb4be842ff
Alan Rominger <arominge@redhat.com>
2022-10-20 11:33:36 -0400

    Add back condition about canceling sibling inventory updates

--- awx/main/models/inventory.py
+++ awx/main/models/inventory.py
@@ -1279,6 +1279,13 @@ class InventoryUpdate(UnifiedJob, InventorySourceOptions, JobNotificationMixin, 
         r = super().get_cancel_chain()
         if self.source_project_update_id:
             r.append(self.source_project_update)
+        # Special case for multiple inventory updates in same inventory
+        # If other sources in this same inventory are running as dependencies of the same job, cancel them too
+        job_id = self.unifiedjob_blocked_jobs.filter(job__isnull=False).values_list('pk', flat=True).first()
+        if job_id:
+            for other_inv_update in InventoryUpdate.objects.filter(unifiedjob_blocked_jobs=job_id, inventory_id=self.inventory_id):
+                if other_inv_update not in r:
+                    r.append(other_inv_update)
         return r
 
     def dependent_templates(self):
