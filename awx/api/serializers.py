8fd1eedd3054c726b9883cec8aa16f26ac5d5f3b
Alan Rominger <arominge@redhat.com>
2023-02-10 11:11:48 -0500

    Avoid contextually inappropriate references to UJT.last_job
    When a job has a important status change
      meaning going from an non-active to active status
      or going from active to completed status
    then we called a job method to update its related template fields
    that then triggered another redundant method as part of the
      update logic in the template
    
    This moves all of the parent update actions into the job method
      and removes the parent updating method because it is not needed
      and eliminates some potential for errors as signal_start
      triggered errored fetches of last_job
      but signal_start has no need to reference last_job
      so this will stop us from doing that

--- awx/api/serializers.py
+++ awx/api/serializers.py
@@ -3854,7 +3854,7 @@ class WorkflowApprovalListSerializer(WorkflowApprovalSerializer, UnifiedJobListS
 class WorkflowApprovalTemplateSerializer(UnifiedJobTemplateSerializer):
     class Meta:
         model = WorkflowApprovalTemplate
-        fields = ('*', 'timeout', 'name')
+        fields = ('*', 'timeout', 'name', '-execution_environment')
 
     def get_related(self, obj):
         res = super(WorkflowApprovalTemplateSerializer, self).get_related(obj)
