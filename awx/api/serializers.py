73430c57da0dcb5e11dde961dd601ec5ca598eff
Nikhil <jainnikhil30@gmail.com>
2023-02-16 16:11:08 +0530

    fix the models on the bulk api serializers

--- awx/api/serializers.py
+++ awx/api/serializers.py
@@ -1991,6 +1991,7 @@ class BulkHostCreateSerializer(serializers.Serializer):
     )
 
     class Meta:
+        model = Inventory
         fields = ('inventory', 'hosts')
         read_only_fields = ()
 
@@ -4579,6 +4580,7 @@ class BulkJobNodeSerializer(serializers.Serializer):
     extra_data = serializers.JSONField(write_only=True, required=False)
 
     class Meta:
+        model = WorkflowJobNode
         fields = (
             'unified_job_template',
             'identifier',
