9691412c4b29349adc69190723bd678c312895a5
Nikhil <jainnikhil30@gmail.com>
2023-02-16 08:57:51 +0530

    add the description for the description field

--- awx/api/serializers.py
+++ awx/api/serializers.py
@@ -4580,6 +4580,7 @@ class BulkJobNodeSerializer(serializers.Serializer):
     extra_data = serializers.JSONField(write_only=True, required=False)
 
     class Meta:
+        model = Work
         fields = (
             'unified_job_template',
             'identifier',
