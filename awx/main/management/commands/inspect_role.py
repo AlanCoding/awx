59d736951269fbed23c3c79d91a8024a8da7b98d
Alan Rominger <arominge@redhat.com>
2023-08-22 11:37:04 -0400

    Add asthetic touchups

--- awx/main/management/commands/inspect_role.py
+++ awx/main/management/commands/inspect_role.py
@@ -10,45 +10,39 @@ from awx.main.models import Role
 
 
 def format_role(role):
-    return '{}-{}-{}'.format(
-        getattr(role.content_type, 'model', None),
-        role.role_field,
-        role.pk
-    )
+    if role.content_type is None:
+        return f'{role.role_field}-{role.pk}'
+    obj_type = role.content_type.name.title()
+    ret = f'{obj_type}-{role.object_id}\n{role.role_field}-{role.pk}'
+    users = list(role.members.values_list('username', flat=True))
+    if users:
+        ret += '\nUSERS\n' + '\n'.join([f'-{un}' for un in users])
+    return ret
 
 
 class Command(BaseCommand):
-
     def add_arguments(self, parser):
-        parser.add_argument('--role', dest='role', type=int, default=None,
-                            help='The role to inspect.')
-        parser.add_argument('--file', dest='file', type=str, default='output',
-                            help='Where to write file to (do not include extension)')
-        parser.add_argument('--method', dest='method', type=str, default='up',
-                            choices=['up', 'down', 'downup'],
-                            help='What directions to crawl to find the graph nodes.')
-        parser.add_argument('--ext', dest='ext', type=str, default='png',
-                            help='Extension')
+        parser.add_argument('--role', dest='role', type=int, default=None, help='The role to inspect.')
+        parser.add_argument('--file', dest='file', type=str, default='output', help='Where to write file to (do not include extension)')
+        parser.add_argument(
+            '--method', dest='method', type=str, default='up', choices=['up', 'down', 'downup'], help='What directions to crawl to find the graph nodes.'
+        )
+        parser.add_argument('--ext', dest='ext', type=str, default='png', help='Extension')
 
     def handle(self, **options):
         role_pk = int(options['role'])
         file = options['file']
+        ext = options['ext']
         method = options['method']
         try:
             from graphviz import Digraph
         except ModuleNotFoundError as e:
-            raise CommandError('You need graphviz to run this command, `pip install graphviz` {}'.format(e))
+            raise CommandError(f'You need graphviz to run this command, error: {e}' '\n\ndnf install graphviz\n/var/lib/awx/venv/awx/bin/pip install graphviz')
 
-        dot = Digraph(
-            format=options['ext'],
-            comment='Ancestry structure for role={}'.format(role_pk)
-        )
+        dot = Digraph(format=ext, comment='Ancestry structure for role={}'.format(role_pk))
         dot.graph_attr['rankdir'] = 'LR'
 
-        role = Role.objects.prefetch_related(
-            'ancestors__content_type', 'parents',
-            'ancestors__parents'
-        ).get(pk=role_pk)
+        role = Role.objects.prefetch_related('ancestors__content_type', 'parents', 'ancestors__parents').get(pk=role_pk)
         edges = []
         nodes = []
 
@@ -68,9 +62,7 @@ class Command(BaseCommand):
         for r in nodes_qs.distinct():
             nodes.append(format_role(r))
             for p in r.parents.all():
-                edges.append(
-                    (format_role(r), format_role(p))
-                )
+                edges.append((format_role(r), format_role(p)))
 
         for node in nodes:
             dot.node(node, node)
@@ -79,3 +71,4 @@ class Command(BaseCommand):
 
         print(dot.source)
         dot.render(file, view=False)
+        print(f'Output has been written to file {file} and {file}.{ext}')
