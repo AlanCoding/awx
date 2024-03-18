7f349fb6c2ad0a9b2102f6a23ba5bd89c173f46b
Alan Rominger <arominge@redhat.com>
2024-03-18 16:21:12 -0400

    Generalize can_delete solution, use devel DAB

--- awx/main/access.py
+++ awx/main/access.py
@@ -264,7 +264,9 @@ class BaseAccess(object):
         return self.can_change(obj, data)
 
     def can_delete(self, obj):
-        return self.user.has_obj_perm(obj, 'delete')
+        if obj._meta.model in [cls._meta.model_name for cls in permission_registry.all_registered_models]:
+            return self.user.has_obj_perm(obj, 'delete')
+        return self.user.is_superuser
 
     def can_copy(self, obj):
         return self.can_add({'reference_obj': obj})
@@ -2934,9 +2936,6 @@ class WorkflowApprovalAccess(BaseAccess):
     def can_approve_or_deny(self, obj):
         if (obj.workflow_job_template and self.user in obj.workflow_job_template.approval_role) or self.user.is_superuser:
             return True
-
-    def can_delete(self, obj):
-        return self.user.is_superuser  # Not really supposed to be done
 
 
 class WorkflowApprovalTemplateAccess(BaseAccess):
