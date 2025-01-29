5b5291c303686cf74d1cc85bfc88c20121543ca8
Bruno Rocha <rochacbruno@gmail.com>
2025-01-29 21:04:09 +0000

    prod settings check

--- awx/settings/__init__.py
+++ awx/settings/__init__.py
@@ -1,5 +1,6 @@
 # Copyright (c) 2015 Ansible, Inc.
 # All Rights Reserved.
+import os
 from ansible_base.lib.dynamic_config import factory, export, load_standard_settings_files
 from .application_name import merge_application_name
 from dynaconf import Validator
@@ -28,8 +29,18 @@ if DYNACONF.is_development_mode:
 ################################################################################################
 
 # Load extra config
-DYNACONF.load_file("/etc/tower/settings.py")
-DYNACONF.load_file("/etc/tower/conf.d/*.py")
+# Load settings from any .py files in the global conf.d directory specified in
+# the environment, defaulting to /etc/tower/conf.d/.
+settings_dir = os.environ.get('AWX_SETTINGS_DIR', '/etc/tower/conf.d/')
+settings_files_path = os.path.join(settings_dir, '*.py')
+# Load remaining settings from the global settings file specified in the
+# environment, defaulting to /etc/tower/settings.py.
+settings_file_path = os.environ.get('AWX_SETTINGS_FILE', '/etc/tower/settings.py')
+# Attempt to load settings from /etc/tower/settings.py first, followed by
+# /etc/tower/conf.d/*.py.
+DYNACONF.load_file(settings_file_path)
+DYNACONF.load_file(settings_files_path)
+
 if DYNACONF.get_environ("AWX_KUBE_DEVEL"):
     DYNACONF.load_file("kube_defaults.py")
 else:
@@ -37,6 +48,27 @@ else:
 
 # Load new standard settings files from /etc/ansible-automation-platform/config/awx/
 load_standard_settings_files(DYNACONF)
+
+# Check at least one required setting file has been loaded
+# NOTE: This potentially could be moved to a validator
+if "production" in DYNACONF.current_env.lower():
+    required_settings_paths = [
+        os.path.dirname(settings_file_path),
+        "/etc/ansible-automation-platform/",
+    ]
+    # check if at least one file has been loaded any of the required paths
+    # use DYNACONF._loaded_files to check any filename inside the paths
+    # if not loaded then raise an ImproperlyConfigured error
+    for path in required_settings_paths:
+        if any(path in f for f in DYNACONF._loaded_files):
+            break
+    else:
+        from django.core.exceptions import ImproperlyConfigured
+
+        msg = 'No AWX configuration found at %s.' % required_settings_paths
+        msg += '\nDefine the AWX_SETTINGS_FILE environment variable to '
+        msg += 'specify an alternate path.'
+        raise ImproperlyConfigured(msg)
 
 # Load envvars at the end to allow them to override everything
 env_loader.load(DYNACONF, identifier="awx.settings")
