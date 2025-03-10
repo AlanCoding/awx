554c08172d6b716d2555f83e8ef9ab1a9869e919
AlanCoding <arominge@redhat.com>
2025-03-10 13:28:53 -0400

    Make basic --status and --running commands work

--- awx/main/apps.py
+++ awx/main/apps.py
@@ -103,7 +103,7 @@ class MainConfig(AppConfig):
                         "config": get_pg_notify_params(),
                         "sync_connection_factory": "awx.main.utils.db.psycopg_connection_from_django",
                         "channels": ['tower_broadcast_all', 'tower_settings_change', get_task_queuename()],
-                        # "default_publish_channel": "tower_broadcast_all"
+                        "default_publish_channel": settings.CLUSTER_HOST_ID,  # used for debugging commands
                     }
                 },
                 "producers": {
