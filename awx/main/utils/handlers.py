62fdd46909c2e5abfa942052892365909e6e071f
AlanCoding <arominge@redhat.com>
2025-05-22 09:32:08 -0400

    Check inventory update cancel_callback more often

--- awx/main/utils/handlers.py
+++ awx/main/utils/handlers.py
@@ -90,10 +90,8 @@ class SpecialInventoryHandler(logging.Handler):
     def emit(self, record):
         # check cancel and timeout status regardless of log level
         this_time = now()
-        if (this_time - self.last_check).total_seconds() > 0.1:
-            self.last_check = this_time
-            if self.cancel_callback():
-                raise PostRunError('Inventory update has been canceled', status='canceled')
+        if self.cancel_callback():
+            raise PostRunError('Inventory update has been canceled', status='canceled')
         if self.job_timeout and ((this_time - self.job_start).total_seconds() > self.job_timeout):
             raise PostRunError('Inventory update has timed out', status='canceled')
 
