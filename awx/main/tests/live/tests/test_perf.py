2b4c9bef04f440b8cd881a473761e40575e23be8
AlanCoding <arominge@redhat.com>
2025-11-25 10:26:25 -0500

    Workflow test

--- awx/main/tests/live/tests/test_perf.py
+++ awx/main/tests/live/tests/test_perf.py
@@ -1,14 +1,24 @@
 import time
 import statistics
 
-from awx.main.models import Job, JobTemplate
+import pytest
+
+from django.test import override_settings
+
+from awx.api.versioning import reverse
+
+from awx.main.models import Job, JobTemplate, WorkflowJob
 
 from awx.main.tests.live.tests.conftest import wait_for_job
 
-N = 1000
+N = 100
+
+N_series = [1, 5, 10, 20, 30, 45, 50, 75, 100, 150, 200]
+# N_series = [10]
 
 
-def test_many_fast_launch(demo_inv, project_factory, live_tmp_folder, admin):
+@pytest.fixture
+def perf_jt(demo_inv, project_factory, live_tmp_folder, admin):
     jt = JobTemplate.objects.filter(name='test_many_fast_launch').first()
     if jt:
         jt.delete()
@@ -24,11 +34,14 @@ def test_many_fast_launch(demo_inv, project_factory, live_tmp_folder, admin):
     jt_data = {'project': proj, 'playbook': 'debug.yml', 'inventory': demo_inv, 'allow_simultaneous': True, 'created_by': admin, 'modified_by': admin}
 
     jt, created = JobTemplate.objects.get_or_create(name='test_many_fast_launch', defaults=jt_data)
+    return jt
 
+
+def test_many_fast_launch(perf_jt):
     jobs = []
     for i in range(N):
         print(f'creating {i}')
-        job = jt.create_unified_job()
+        job = perf_jt.create_unified_job()
         jobs.append(job)
 
     for i, job in enumerate(jobs):
@@ -50,3 +63,20 @@ def test_many_fast_launch(demo_inv, project_factory, live_tmp_folder, admin):
     print(' '.join([str(t) for t in run_times]))
 
     raise Exception({'start_mean': statistics.mean(start_times), 'run_mean': statistics.mean(run_times)})
+
+
+def test_workflow_bulk_launch(perf_jt, post, admin):
+    print('N      time')
+    for N_local in N_series:
+        with override_settings(BULK_JOB_MAX_LAUNCH=N_local + 1):
+            jobs = [{'unified_job_template': perf_jt.id} for _ in range(N_local)]
+
+            response = post(url=reverse('api:bulk_job_launch'), data={'name': 'Bulk Job Launch', 'jobs': jobs}, user=admin, expect=201)
+
+            data = response.data
+            wj_id = data['id']
+            wj = WorkflowJob.objects.get(id=wj_id)
+            wait_for_job(wj)
+            delta = (wj.finished - wj.created).total_seconds()
+            print(f'{N_local}    {delta}')
+    raise Exception('alan!')
